0dcf1a61dfa36cee4d11590ee00aef54f561b492
bagavievbulat <buba1219@yandex.ru>
2021-09-18 17:01:26 +0300

    1) Начало положено: фон, красивое поле.

---
+++ Homework/Lesson5/app/src/main/java/com/example/lesson5/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.lesson5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // imageButton clicklistener
+    // imageButton statelist
+
+
+}
