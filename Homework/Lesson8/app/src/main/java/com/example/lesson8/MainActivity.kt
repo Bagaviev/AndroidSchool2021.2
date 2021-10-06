525b340a98ea5211e8acc2acb7d5714ffa7e5d12
bagavievbulat <buba1219@yandex.ru>
2021-10-06 22:03:39 +0300

    2) рисовка на холсте и костыльная реализация стрелки (какая есть)

--- Homework/Lesson8/app/src/main/java/com/example/lesson8/MainActivity.kt
+++ Homework/Lesson8/app/src/main/java/com/example/lesson8/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.lesson8
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    lateinit var seekBar: SeekBar
+    lateinit var speedometer: Speedometer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        speedometer = findViewById(R.id.speedometer)
+
+        seekBar = findViewById(R.id.seekBar)
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                seekBar?.max = 200
+                speedometer.setSpeed(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
--- Homework/Lesson8/app/src/main/java/com/example/lesson8/Speedometer.kt
+++ Homework/Lesson8/app/src/main/java/com/example/lesson8/Speedometer.kt
@@ -1,7 +1,9 @@
 package com.example.lesson8
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
 import android.os.Build
 import android.util.AttributeSet
 import android.view.View
@@ -14,32 +16,42 @@ import androidx.annotation.RequiresApi
  */
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-class Speedometer(context: Context, attributeSet: AttributeSet?, defStyleAttrs: Int, defStyleRes: Int) :
-      View(context, attributeSet, defStyleAttrs, defStyleRes) {
-
-    var speed: Int = 0
-    var maxSpeed: Int = 200
+class Speedometer(context: Context, attributeSet: AttributeSet?) : View(context, attributeSet) {    // бага была - падает с малоинф ошибкой если указаны defStyleAttrs: Int, defStyleRes: Int
+    private var speed: Int
+    private var maxSpeed: Int
 
     @ColorInt
-    var lineColor: Int = Color.GRAY
+    private var lineColor: Int
     @ColorInt
-    var lowSpeedTextColor: Int = Color.GREEN
+    private var lowSpeedTextColor: Int
     @ColorInt
-    var medSpeedTextColor: Int = Color.YELLOW
+    private var medSpeedTextColor: Int
     @ColorInt
-    var hiSpeedTextColor: Int = Color.RED
+    private var hiSpeedTextColor: Int
+
+    val textPainter = Paint(Paint.ANTI_ALIAS_FLAG)
+    val linePainter = Paint(Paint.ANTI_ALIAS_FLAG).apply { strokeWidth = 8f }
+
+    var mainArc = Paint(Paint.ANTI_ALIAS_FLAG).apply {    // удобно, подсказка параметров появляется при наборе в пустоту
+        style = Paint.Style.STROKE
+        strokeWidth = 16f
+    }
+
+    var smalArc = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+    }
 
     init {
         val typedArray = context.theme.obtainStyledAttributes(
             attributeSet,
             R.styleable.Speedometer,
-            R.attr.speedometerDefaultAttr,
-            R.style.SpeedometerDefaultStyle
+            0,      // R.attr.speedometerDefaultAttr,
+            0       // R.style.SpeedometerDefaultStyle
         )
 
         try {
             speed = typedArray.getInt(R.styleable.Speedometer_speed, 0)
-            maxSpeed = typedArray.getInt(R.styleable.Speedometer_maxSpeed, 180)
+            maxSpeed = typedArray.getInt(R.styleable.Speedometer_maxSpeed, 0)
 
             lineColor = typedArray.getColor(R.styleable.Speedometer_lineColor, Color.GRAY)
             lowSpeedTextColor = typedArray.getColor(R.styleable.Speedometer_lowSpeedTextColor, Color.GREEN)
@@ -47,6 +59,25 @@ class Speedometer(context: Context, attributeSet: AttributeSet?, defStyleAttrs: 
             hiSpeedTextColor = typedArray.getColor(R.styleable.Speedometer_hiSpeedTextColor, Color.RED)
         } finally {
             typedArray.recycle()
+        }
+
+        mainArc.color = lineColor
+        linePainter.color = lineColor
+        smalArc.color = lineColor
+        // тут конфигим Paint потом
+    }
+
+    fun setSpeed(newSpeed: Int) {
+        speed = newSpeed
+        invalidate()
+    }
+
+    override fun onDraw(canvas: Canvas?) {      // drawLine(200f, 200f, 270f, 30f, linePainter)     // полярные координаты не отрабатывали как нужно
+        canvas?.apply {
+            translate(8f, 8f)
+            drawArc(0f, 0f, 400f, 400f, 180f, 180f, true, mainArc)
+            drawArc(0f, 0f, 400f, 400f, 180f / maxSpeed * speed + 180f, 2f, true, linePainter)
+            drawArc(150f, 150f, 250f, 250f, 180f, 180f, true, smalArc)
         }
     }
 }
