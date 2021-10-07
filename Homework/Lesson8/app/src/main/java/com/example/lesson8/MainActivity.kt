3620ba152b3312dadc4c0aef4fed45d3ec8fb584
bagavievbulat <buba1219@yandex.ru>
2021-10-07 22:26:31 +0300

    3) Все готово формально, потом мб resize сделаю.

--- Homework/Lesson8/app/src/main/java/com/example/lesson8/MainActivity.kt
+++ Homework/Lesson8/app/src/main/java/com/example/lesson8/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.lesson8
 
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
 import android.widget.SeekBar
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.red
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var seekBar: SeekBar
@@ -20,12 +24,26 @@ class MainActivity : AppCompatActivity() {
         seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
             @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                seekBar?.max = 200
+                seekBar?.max = Speedometer.maxSpeed
                 speedometer.setSpeed(progress)
+
+                if (Speedometer.currentSpeed <= Speedometer.maxSpeed / 3) {
+                    speedometer.speedTextPainter.color = Speedometer.lowSpeedTextColor
+                } else if (Speedometer.currentSpeed <= 2 * Speedometer.maxSpeed / 3) {
+                    speedometer.speedTextPainter.color = Speedometer.medSpeedTextColor
+                } else {
+                    speedometer.speedTextPainter.color = Speedometer.hiSpeedTextColor
+                }
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
     }
+
+    /*private fun resolveMaxSpeed() {      // вернуло 0 хз почему видимо не узнаем
+        val typedValue = TypedValue()
+        speedometer.context.theme.resolveAttribute(R.styleable.Speedometer_maxSpeed, typedValue, true)
+        Log.e("Logs", "xml value: ${typedValue.float}")
+    }*/
 }
--- Homework/Lesson8/app/src/main/java/com/example/lesson8/Speedometer.kt
+++ Homework/Lesson8/app/src/main/java/com/example/lesson8/Speedometer.kt
@@ -1,14 +1,14 @@
 package com.example.lesson8
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.os.Build
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.ColorInt
 import androidx.annotation.RequiresApi
+import androidx.core.content.res.ResourcesCompat
+import kotlin.coroutines.coroutineContext
 
 /**
  * @author Bulat Bagaviev
@@ -17,67 +17,107 @@ import androidx.annotation.RequiresApi
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class Speedometer(context: Context, attributeSet: AttributeSet?) : View(context, attributeSet) {    // бага была - падает с малоинф ошибкой если указаны defStyleAttrs: Int, defStyleRes: Int
-    private var speed: Int
-    private var maxSpeed: Int
 
-    @ColorInt
-    private var lineColor: Int
-    @ColorInt
-    private var lowSpeedTextColor: Int
-    @ColorInt
-    private var medSpeedTextColor: Int
-    @ColorInt
-    private var hiSpeedTextColor: Int
+    companion object {
+        val SMALL_BORDER_MARGIN = 8f
+        val ARROW_WIDTH = 2f
+        val placehoderList = mutableListOf<String>()
+        var currentSpeed: Int = 0
+        var maxSpeed: Int = 0
 
-    val textPainter = Paint(Paint.ANTI_ALIAS_FLAG)
-    val linePainter = Paint(Paint.ANTI_ALIAS_FLAG).apply { strokeWidth = 8f }
+        var lineColor: Int = 0
+        var lowSpeedTextColor: Int = 0
+        var medSpeedTextColor: Int = 0
+        var hiSpeedTextColor: Int = 0
+        var mainColor: Int = Color.parseColor("#906aad")
+    }
 
-    var mainArc = Paint(Paint.ANTI_ALIAS_FLAG).apply {    // удобно, подсказка параметров появляется при наборе в пустоту
+    val cSpeedRect = Rect()
+
+    val mainArcRect = RectF(0f, 0f, 400f, 400f)
+
+    val smallArcRect = RectF(150f, 150f, 250f, 250f)
+
+    val additionalTextPainter = Paint(Paint.ANTI_ALIAS_FLAG).apply { textSize = 25f }
+
+    val speedTextPainter = Paint(Paint.ANTI_ALIAS_FLAG).apply { textSize = 35f }
+
+    val arrowPainter = Paint(Paint.ANTI_ALIAS_FLAG).apply { strokeWidth = 8f }
+
+    val mainArcPainter = Paint(Paint.ANTI_ALIAS_FLAG).apply {    // удобно, подсказка параметров появляется при наборе в пустоту
         style = Paint.Style.STROKE
         strokeWidth = 16f
     }
 
-    var smalArc = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        style = Paint.Style.FILL
-    }
+    var smalArcPainter = Paint(Paint.ANTI_ALIAS_FLAG).apply { style = Paint.Style.FILL }
 
     init {
+        repeat(6) { placehoderList.add(String()) }      // text0, text1, text2, text3, text4, textCs
+
         val typedArray = context.theme.obtainStyledAttributes(
             attributeSet,
             R.styleable.Speedometer,
-            0,      // R.attr.speedometerDefaultAttr,
-            0       // R.style.SpeedometerDefaultStyle
+            R.attr.speedometerDefaultAttr,
+            R.style.SpeedometerDefaultStyle
         )
 
         try {
-            speed = typedArray.getInt(R.styleable.Speedometer_speed, 0)
+            currentSpeed = typedArray.getInt(R.styleable.Speedometer_speed, 0)
             maxSpeed = typedArray.getInt(R.styleable.Speedometer_maxSpeed, 0)
-
-            lineColor = typedArray.getColor(R.styleable.Speedometer_lineColor, Color.GRAY)
-            lowSpeedTextColor = typedArray.getColor(R.styleable.Speedometer_lowSpeedTextColor, Color.GREEN)
-            medSpeedTextColor = typedArray.getColor(R.styleable.Speedometer_medSpeedTextColor, Color.YELLOW)
-            hiSpeedTextColor = typedArray.getColor(R.styleable.Speedometer_hiSpeedTextColor, Color.RED)
+            lineColor = typedArray.getColor(R.styleable.Speedometer_lineColor, 0)
+            lowSpeedTextColor = typedArray.getColor(R.styleable.Speedometer_lowSpeedTextColor, 0)
+            medSpeedTextColor = typedArray.getColor(R.styleable.Speedometer_medSpeedTextColor, 0)
+            hiSpeedTextColor = typedArray.getColor(R.styleable.Speedometer_hiSpeedTextColor, 0)
         } finally {
             typedArray.recycle()
         }
 
-        mainArc.color = lineColor
-        linePainter.color = lineColor
-        smalArc.color = lineColor
-        // тут конфигим Paint потом
+        mainArcPainter.color = mainColor
+        smalArcPainter.color = mainColor
+        arrowPainter.color = lineColor
+        speedTextPainter.color = lowSpeedTextColor
     }
 
     fun setSpeed(newSpeed: Int) {
-        speed = newSpeed
+        currentSpeed = newSpeed
         invalidate()
     }
 
     override fun onDraw(canvas: Canvas?) {      // drawLine(200f, 200f, 270f, 30f, linePainter)     // полярные координаты не отрабатывали как нужно
         canvas?.apply {
-            translate(8f, 8f)
-            drawArc(0f, 0f, 400f, 400f, 180f, 180f, true, mainArc)
-            drawArc(0f, 0f, 400f, 400f, 180f / maxSpeed * speed + 180f, 2f, true, linePainter)
-            drawArc(150f, 150f, 250f, 250f, 180f, 180f, true, smalArc)
+            translate(SMALL_BORDER_MARGIN, SMALL_BORDER_MARGIN)
+            drawArc(mainArcRect, 180f, 180f, true, mainArcPainter)
+            drawArc(mainArcRect, 180f / maxSpeed * currentSpeed + 180f, ARROW_WIDTH, true, arrowPainter)
+            drawArc(smallArcRect, 180f, 180f, true, smalArcPainter)
+
+            initText()
+        }
+    }
+
+    private fun Canvas.initText() {     // extension
+        setSpeedPlaceholders(currentSpeed, maxSpeed)
+
+        drawText(placehoderList[0], 20f, 185f, additionalTextPainter)
+        drawText(placehoderList[1], 70f, 90f, additionalTextPainter)
+        drawText(placehoderList[2], 180f, 40f, additionalTextPainter)
+        drawText(placehoderList[3], 290f, 90f, additionalTextPainter)
+        drawText(placehoderList[4], 340f, 185f, additionalTextPainter)
+
+        val s = "$currentSpeed km/h"
+        speedTextPainter.getTextBounds(s, 0, s.length, cSpeedRect)
+        drawText(s, 200f - cSpeedRect.centerX(), 100f - cSpeedRect.centerY(), speedTextPainter)
+    }
+
+    fun setSpeedPlaceholders(speed: Int, maxSpeed: Int) {       // четенько получилось с первого раза
+        placehoderList[0] = "0"
+        placehoderList[4] = maxSpeed.toString()
+        placehoderList[5] = speed.toString()
+
+        var maxSpeedTmp = maxSpeed
+
+        for (i in 3 downTo 0) {
+            maxSpeedTmp -= maxSpeed / 4
+            placehoderList[i] = maxSpeedTmp.toString()
         }
     }
 }
