9bfdb3f5395016036ae4dcf069f3f3223b9ba8af
bagavievbulat <buba1219@yandex.ru>
2021-10-30 16:05:46 +0300

    3) OkHttp готов, далее Moshi.

--- Homework/Lesson17/app/src/main/java/com/example/lesson17/net/connectors/OkHttpConnector.kt
+++ Homework/Lesson17/app/src/main/java/com/example/lesson17/net/connectors/OkHttpConnector.kt
@@ -1,7 +1,15 @@
 package com.example.lesson17.net.connectors
 
 import com.example.lesson17.net.NetConnector
+import com.example.lesson17.net.NetworkRepository
 import com.example.lesson17.pojo.User
+import com.example.lesson17.utils.DataConverter
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
 
 /**
  * @author Bulat Bagaviev
@@ -9,12 +17,37 @@ import com.example.lesson17.pojo.User
  */
 
 class OkHttpConnector: NetConnector {
+
     override fun doGet(): User? {
-        return User(1,1,"1", true)
+        var okHttpClient = OkHttpClient()
+
+        val request: Request = Request.Builder()
+            .url(NetworkRepository.URL)
+            .build()
+
+        val response: Response = okHttpClient.newCall(request).execute()
+        val result: String = response.body!!.string()
+
+        return DataConverter.convertFromJson(result)
     }
 
     override fun doPost(): User? {
-        return User(1,1,"1", true)
+        var okHttpClient = OkHttpClient()
+        val user = User(1, 1, "UPD", false)
+        val json: String = DataConverter.convertToJson(user).toString()
+
+        val body: RequestBody = json
+            .toRequestBody("application/json".toMediaTypeOrNull())
+
+        val request: Request = Request.Builder()
+            .url(NetworkRepository.URL)
+            .method("PUT", body)
+            .addHeader("Content-type", "application/json; charset=UTF-8")
+            .build()
+
+        val response: Response = okHttpClient.newCall(request).execute()
+        val result: String = response.body!!.string()
+        return DataConverter.convertFromJson(result)
     }
 
     override fun toString(): String {
